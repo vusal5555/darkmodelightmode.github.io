841e6e1b394333029a93e8d3de2682565264d078
vusal5555 <vusal.novruzov.1@iliauni.edu.ge>
2023-03-04 00:42:28 -0800

    secondary commit

--- script.js
+++ script.js
@@ -6,37 +6,53 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+//image path change
 const imageChange = function (color) {
   image1.src = `img/undraw_proud_coder_${color}.svg`;
   image2.src = `img/undraw_feeling_proud_${color}.svg`;
   image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 };
-const darkMode = function () {
-  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-  imageChange('dark');
-};
-const lightMode = function () {
-  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-  imageChange('light');
+
+//clean code
+const toggleDarkLightMode = function (isDark) {
+  nav.style.backgroundColor = isDark
+    ? 'rgb(255 255 255 / 50%)'
+    : 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+  isDark
+    ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  isDark ? imageChange('dark') : imageChange('dark');
 };
 
+//Not clean code
+// const darkMode = function () {
+//   nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+//   toggleIcon.children[0].textContent = 'Dark Mode';
+//   toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+// };
+// const lightMode = function () {
+//   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+//   toggleIcon.children[0].textContent = 'Light Mode';
+//   toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+//   imageChange('light');
+// };
+
+//main function
 const themeChange = function (e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('data-theme', 'dark');
-    darkMode();
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('data-theme', 'light');
-    lightMode();
+    toggleDarkLightMode(false);
   }
 };
 toggleSwitch.addEventListener('change', themeChange);
 
+//local storage to keep user preference
 const curTheme = localStorage.getItem('data-theme');
 
 if (curTheme) {
@@ -44,8 +60,8 @@ if (curTheme) {
 
   if (curTheme === 'dark') {
     toggleSwitch.checked = true;
-    darkMode();
+    toggleDarkLightMode(true);
   } else {
-    lightMode();
+    toggleDarkLightMode(false);
   }
 }
